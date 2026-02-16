0361b5170009cd709ceb54030c02fd853f46b076
AleOjeda01 <aleoe.12@gmail.com>
2026-02-16 17:44:50 -0300

    cartel de confirmacion - evito reedireccion

--- js/script.js
+++ js/script.js
@@ -6,6 +6,40 @@ const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("mensaje");
 
+formulario.addEventListener("submit", function (e) {
+
+  e.preventDefault(); // prevenimos envío tradicional
+
+  const datos = new FormData(formulario);
+
+  fetch("https://formspree.io/f/xvzbanae", {
+    method: "POST",
+    body: datos,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        formulario.reset();
+
+        const exito = document.getElementById("mensajeEnviado");
+        exito.classList.add("mostrar");
+
+        setTimeout(() => {
+          exito.classList.remove("mostar");
+        }, 4000);
+
+      } else {
+        alert("Hubo un error al enviar el mensaje.");
+      }
+    })
+    .catch((error) => {
+      alert("Error de conexión.");
+    });
+});
+
+// Pintar de rojo campos vacios
 nombre.addEventListener("invalid", function () {
   nombre.classList.add("error-input");
 });
