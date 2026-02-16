8ded7e9c17c4a9ff5d5d9ccce2da299d446bcf3e
AleOjeda01 <aleoe.12@gmail.com>
2026-02-16 16:56:27 -0300

    campos vacios pintados de rojo

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
+console.log("JS funcionando");
+
 const formulario = document.getElementById("formulario");
 
 const nombre = document.getElementById("nombre");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("mensaje");
 
-formulario.addEventListener("submit", function (e) {
-  let hayError = false;
+nombre.addEventListener("invalid", function () {
+  nombre.classList.add("error-input");
+});
 
-  // Validar nombre
-  if (nombre.value.trim() === "") {
-    nombre.classList.add("error-input");
-    hayError = true;
-  } else {
+email.addEventListener("invalid", function () {
+  email.classList.add("error-input");
+});
+
+mensaje.addEventListener("invalid", function () {
+  mensaje.classList.add("error-input");
+});
+
+// Quitar rojo cuando se corrige
+nombre.addEventListener("input", function () {
+  if (nombre.checkValidity()) {
     nombre.classList.remove("error-input");
   }
+});
 
-  // Validar email
-  if (email.value.trim() === "" || !email.checkValidity()) {
-    email.classList.add("error-input");
-    hayError = true;
-  } else {
+email.addEventListener("input", function () {
+  if (email.checkValidity()) {
     email.classList.remove("error-input");
   }
+});
 
-  // Validar mensaje
-  if (mensaje.value.trim() === "") {
-    mensaje.classList.add("error-input");
-    hayError = true;
-  } else {
+mensaje.addEventListener("input", function () {
+  if (mensaje.checkValidity()) {
     mensaje.classList.remove("error-input");
-  }
-
-  // Si hay error, no se envía el formulario
-  if (hayError) {
-    e.preventDefault();
   }
 });
